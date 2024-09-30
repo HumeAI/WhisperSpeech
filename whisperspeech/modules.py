706587d01315537f4215bace5e18b92ea54cfb2e
Jakub Piotr Cłapa <jakub@hume.ai>
2024-09-30 22:25:15 +0000

    s2a: compile both prefill and the encoder, preallocate all RoPE buffers for that to work

--- whisperspeech/modules.py
+++ whisperspeech/modules.py
@@ -160,22 +160,23 @@ class Rotary(torch.nn.Module):
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
         self.register_buffer("inv_freq", inv_freq)
         self.seq_len_cached = None
-        self.cos_cached = None
-        self.sin_cached = None
+        # self.cos_cached = None
+        # self.sin_cached = None
 
-    def forward(self, x, seq_dim=1):
-        seq_len = x.shape[seq_dim]
         if not self.seq_len_cached or seq_len > self.seq_len_cached:
+            # print('Rotary', self.seq_len_cached, self.seq_len_cached and seq_len > self.seq_len_cached)
             self.seq_len_cached = 2500
             # self.seq_len_cached = seq_len
             
-            t = torch.arange(self.seq_len_cached, device=x.device).type_as(self.inv_freq)
+            t = torch.arange(self.seq_len_cached).type_as(self.inv_freq)
             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-            self.cos_cached = emb.cos()[None, :, None, :]
-            self.sin_cached = emb.sin()[None, :, None, :]
+            emb = torch.cat((freqs, freqs), dim=-1)
+            self.register_buffer('cos_cached', emb.cos()[None, :, None, :])
+            self.register_buffer('sin_cached', emb.sin()[None, :, None, :])
+    
+    def forward(self, x, seq_dim=1):
+        seq_len = x.shape[seq_dim]
         return self.cos_cached, self.sin_cached
-
 
 # rotary pos emb helpers:
 def rotate_half(x):
